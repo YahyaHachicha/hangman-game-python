31e9da9b4c126532f9233e72af11c6da8d5f7c0c
YahyaHachicha <yahyahachicha04@gmail.com>
2022-08-27 14:32:47 +0000

    created functional

--- hangman.py
+++ hangman.py
@@ -5,3 +5,42 @@ index = randint(0, len(words)-1);
 print (index);
 word = words[index];
 print(word);
+hangman = """
+|-----------------|
+|                 |
+|  |----|         |
+|  |    |         |
+|  |    1         |
+|  |   234        |
+|  |    3         |
+|  |   5 6        |
+|  |              |
+|  |--------      |
+|                 |
+|-----------------|
+"""
+print(hangman)
+
+def print_hangman(errors):
+    hangman = """
+|-----------------|
+|                 |
+|  |----|         |
+|  |    |         |
+|  |    1         |
+|  |   234        |
+|  |    3         |
+|  |   5 6        |
+|  |              |
+|  |--------      |
+|                 |
+|-----------------|
+"""
+    hangman= hangman.replace("1"," ");
+    hangman= hangman.replace("2"," ");
+    hangman= hangman.replace("3"," ");
+    hangman= hangman.replace("4"," ");
+    hangman= hangman.replace("5"," ");
+    hangman= hangman.replace("6"," ");
+    print(hangman);
+print_hangman(3);
